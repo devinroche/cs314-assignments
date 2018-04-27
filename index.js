79e240cf2999e4f50ed69292ab37489921536799
devin <roche.devin@ymail.com>
2018-04-27 10:10:28 -0700

    done

--- index.js
+++ index.js
@@ -1,31 +1,101 @@
-let scroll2section = (section) => section.scrollIntoView()
+let todos
+let albums
+let showTodos = false
+let showAlbums = false
 
-let setBg = (color) => document.body.style.backgroundColor = color
-
-let currColor = ''
-let content = 'Click for '
-
-let poBtnToggle = () => {
-    currColor = currColor == 'pink' && currColor !== '' ? 'orange' : 'pink'
-    let currText = currColor == 'pink' ? 'orange' : 'pink'
-    document.getElementById('poBtn').firstChild.data = content + currText + '!';
-    return document.body.style.backgroundColor = currColor
-}
-
-let getInput = () => {
-    let d = document.getElementById('input3').value
-    let li = document.createElement("li");
-    li.appendChild(document.createTextNode(d));
-    document.getElementById('list').appendChild(li);
-}
-
-let removeLi = (el) => el.parentNode.removeChild(el)
-
-let liBg = (el) => {
-    el.parentNode.childNodes.forEach((curr) => {
-        if(curr.style)
-            curr.style.backgroundColor = 'lightgray'
+$(() => {
+    $.get('https://jsonplaceholder.typicode.com/users')
+        .done((data) => {
+        $.each(data, function(idx, el) {
+            $('#data').append($(`
+            <div class='user' id='main-${el.id}'>
+                ${el.name} <br>
+                ${el.email} <br>
+                ${el.company.name} <br>
+                <button id='${el.id}' class='todoBtn'>To Do</button>
+                <button id='${el.id}' class='albumBtn'>Albums</button>
+                <br><br>
+            </div>`));
+        });
     })
 
-    el.style.backgroundColor = 'rebeccapurple'
+    $('#data').on('click', '.todoBtn', function(){
+        todoDataHandler(this.id)
+    });
+
+    $('#data').on('click', '.albumBtn', function(){
+        albumDataHandler(this.id)
+    });
+})
+
+let hideAlbums = () => $(`.albums`).hide(500)
+let hideTodo = () => $(`.todos`).hide(500)
+let toggleShows = (t, a) => {
+    showTodos = t;
+    showAlbums = a;
+}
+
+let todoDataHandler = (id) => {
+    if(!showTodos) {
+        if(typeof todos === 'undefined') {
+            $.get('https://jsonplaceholder.typicode.com/todos', (todo) => {
+                todos = todo
+                addTodos(todos.filter((el) => el.userId.toString() === id), id)
+            })
+        }
+        else {
+            addTodos(todos.filter((el) => el.userId.toString() === id), id)
+        }
+
+        toggleShows(true, false)
+        hideAlbums()
+    }
+    else { 
+        toggleShows(false, false)
+        hideTodo()
+    }
+}
+
+let albumDataHandler = (id) => {
+    if(!showAlbums) {
+        if(typeof albums === 'undefined') {
+            $.get('https://jsonplaceholder.typicode.com/albums', (album) => {
+                albums = album
+                addAlbums(albums.filter((el) => el.userId.toString() === id), id)
+            })
+        }
+        else {
+            addAlbums(albums.filter((el) => el.userId.toString() === id), id)
+        }
+
+        hideTodo()
+        toggleShows(false, true)
+    }
+    else { 
+        toggleShows(false, false)
+        hideAlbums()
+    }
+}
+
+let addTodos = (userTodos, id) => {
+    $.each(userTodos, (idx, el) => {
+        let isDone = el.completed===true ? "fas fa-check" : ""
+        $(`#main-${id}`).append($(`
+            <div class='todos'>
+                ${el.id}: ${el.title} <i class='${isDone}'></i>
+                <br> <br>
+            </div>
+        `))
+    })
+}
+
+let addAlbums = (userAlbums, id) => {
+    $.each(userAlbums, (idx, el) => {
+        $(`#main-${id}`).append($(`
+            <div class='albums'>
+                ${el.id}: ${el.title}
+                <br> <br>
+            </div>
+        `))
+    })
 }
